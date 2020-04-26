695484300f52ae40a209f8cc0044ffeecd176a0e
HyeIn Ju <hyein.ju92@gmail.com>
2020-04-26 23:42:30 +0900

    Fix: arguments type 추가

--- any-weather-playlist/src/lib/api.ts
+++ any-weather-playlist/src/lib/api.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 
-export const getLatLngAPI = () =>
-    axios.post(`https://www.googleapis.com/geolocation/v1/geolocate?key=${process.env.REACT_APP_GEOLOCATION_API}`);
-export const getWeatherAPI = (lat, lon) =>
+// google cloud platform 키 만료
+// export const getLatLngAPI = () =>
+//     axios.post(`https://www.googleapis.com/geolocation/v1/geolocate?key=${process.env.REACT_APP_GEOLOCATION_API}`);
+
+export const getWeatherAPI = (lat: number, lon: number) =>
     axios.get(
         `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${process.env.REACT_APP_WEATHER_API}`,
     );
