e1d4ddc9ee9f7d3fc2292fd63d438467e94e3a65
HyeIn Ju <hyein.ju92@gmail.com>
2020-04-26 23:24:51 +0900

    Add: eslint, prettier 적용

---
+++ any-weather-playlist/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    semi: true,
+    trailingComma: "all",
+    singleQuote: false,
+    printWidth: 120,
+    tabWidth: 4,
+    orderedImports: true,
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+};
